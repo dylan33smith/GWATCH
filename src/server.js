c1da4933ea1eb16c8a4ad7640fd7087f58317f38
dylan33smith <dylan.m.smith33@gmail.com>
2025-06-22 13:58:20 -0400

    initial commit - created server and mongodb connection

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const { default: mongoose } = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
